1266e858f823d994df127ad921a631679000a99c
Kedar Salunkhe <kedarsalunkhe325@gmail.com>
2023-10-17 18:27:24 +0530

    Adding files
    Signed-off-by: Kedar Salunkhe <kedarsalunkhe325@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-
-from flask import Flask 
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
 
 app = Flask(__name__)
+app.secret_key = "ddfdfhdjfhdjh"
 
-@app.route('/')
-def home():
-    return "Hello this is my branch lelll"
+
+class UserForm(FlaskForm):
+    name = StringField('Name')
+    password = PasswordField('Password')
+    submit = SubmitField("Submit")
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    form = UserForm()
+    if form.validate_on_submit():
+        return redirect("/login")
+    return render_template("login.html", form=form)
+
+@app.route('/login')
+def login_successful():
+    return "login"
 
 if __name__ == "__main__":
     app.debug = True
