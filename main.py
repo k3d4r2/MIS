59f1cba0030548812dc25db3b096d3285d7ca732
Kedar Salunkhe <kedarsalunkhe325@gmail.com>
2023-10-25 13:04:22 +0530

    Add UI components
    Signed-off-by: Kedar Salunkhe <kedarsalunkhe325@gmail.com>

--- main.py
+++ main.py
@@ -1,27 +1,66 @@
-from flask import Flask, render_template, redirect
+from flask import Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField,SubmitField
+from wtforms import DecimalField,EmailField,PasswordField
+from wtforms.validators import DataRequired,EqualTo
+from flask import render_template
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
-app.secret_key = "ddfdfhdjfhdjh"
+app =Flask("HI")
 
 
-class UserForm(FlaskForm):
-    name = StringField('Name')
-    password = PasswordField('Password')
+app.config['SECRET_KEY'] = "hello_everynyan"
+
+
+
+class SignupForm(FlaskForm):
+    name = StringField("Name",validators=[DataRequired()])
+    prn = DecimalField("Prn",validators=[DataRequired()])
+    mail = EmailField("Email",validators=[DataRequired()])
+    password = PasswordField("Password",validators=[DataRequired(),EqualTo(fieldname='confirm',message="password must match")])
+    confirm = PasswordField("Confirm Password",validators=[DataRequired()])
     submit = SubmitField("Submit")
 
-@app.route('/', methods=["POST", "GET"])
-def index():
-    form = UserForm()
+
+class LoginForm(FlaskForm):
+    name = StringField("Name",validators=[DataRequired()])
+    prn = DecimalField("Prn",validators=[DataRequired()])
+    password =PasswordField("Password",validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    name = None
+    prn = None
+    mail = None
+    password= None
+    confirm =None
+    form = SignupForm()
     if form.validate_on_submit():
-        return redirect("/login")
-    return render_template("login.html", form=form)
+        name =form.name.data
+        prn = form.prn.data
+        mail = form.mail.data
+        password =form.password.data
+        confirm = form.confirm.data
+        
+        # DATABASE
+    return render_template('signup.html',name=name,prn=prn,mail=mail,form =form)
+@app.route('/login',methods=['GET','POST'])
+def login():
+    name = None
+    prn = None
+    password =None
+    form = LoginForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        prn = form.name.data
+        password = form.password.data
 
-@app.route('/login')
-def login_successful():
-    return "login"
+        # DATABASE 
+    return render_template("login.html",name=name,prn=prn,form=form)
+    
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+
+
+app.run(debug=True)
