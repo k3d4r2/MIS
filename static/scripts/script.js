14b5a818b45e0565cc4725e17d4d6b42dbb9b0ef
SanketJadhav7d3 <sanketjadhav7d3@gmail.com>
2023-12-12 18:02:55 +0530

    added css loader animation

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -77,7 +77,20 @@ function removeAdmin(uid) {
 function toggleUserProfile(uid) {
     // get element
     const userProfileContainer = document.getElementById("user-" + uid);
+    userProfileContainer.style.opacity = 1;
     userProfileContainer.style.display = (userProfileContainer.style.display === 'none' || userProfileContainer.style.display === '') ? 'flex' : 'none';
+}
+
+function showLoader() {
+    const loader = document.getElementsByClassName("loader")[0];
+    loader.style.display = 'block';
+    console.log("Loader shown")
+}
+
+function hideLoader() {
+    const loader = document.getElementsByClassName("loader")[0];
+    loader.style.display = 'none';
+    console.log("Loader hidden")
 }
 
 function updateUsersList() {
@@ -90,7 +103,15 @@ function updateUsersList() {
         .then(response => {
             return response.text();
         })
+        .then((html) => {
+            showLoader();
+            return html;
+        })
         .then(html => {
             listContainer.innerHTML = html;
-        })
+            return Promise.resolve();
+        }).then(() => {
+            hideLoader();
+        });
 }
+
